d5f95a2e42eef4f60a0e2ffbad3f3303b895de5f
Bruno C Ambrosio <bruno.car.ambrosio@gmail.com>
2019-08-18 18:27:20 -0300

    Atalho command(ou alt) + < para voltar
    Da tela de cadastro pra de login
    Da tela de recuperação de senha pra de login

--- src/main/java/com/cronoteSys/controller/MasterController.java
+++ src/main/java/com/cronoteSys/controller/MasterController.java
@@ -9,6 +9,9 @@ import com.cronoteSys.util.ScreenUtil;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -40,7 +43,7 @@ public class MasterController {
 
 	}
 
-	public static void saveProp(Properties prop)throws IOException {
+	public static void saveProp(Properties prop) throws IOException {
 		FileOutputStream file = new FileOutputStream("./application.properties");
 		prop.store(file, "");
 	}
@@ -50,5 +53,21 @@ public class MasterController {
 	 */
 	public void setsPreviewsScene(String sPreviewsScene) {
 		this.sPreviewsScene = sPreviewsScene;
+
+		btnBack.setMnemonicParsing(true);
+		String OS = System.getProperty("os.name").toLowerCase();
+		KeyCombination kcBack = null;
+		if (OS.indexOf("mac") >= 0) {
+			kcBack = new KeyCodeCombination(KeyCode.LEFT, KeyCombination.META_DOWN);
+		} else {
+			kcBack = new KeyCodeCombination(KeyCode.LEFT, KeyCombination.ALT_DOWN);
+
+		}
+
+		Runnable rn = () -> btnBackClicked();
+		if (btnBack != null) {
+			btnBack.getScene().getAccelerators().putIfAbsent(kcBack, rn);
+		}
 	}
+
 }
